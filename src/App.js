dc95c7c8a8debaff9b27e11bbab1282d83aaff26
rndhldlxp <autarkist@naver.com>
2023-02-04 23:37:12 +0900

    CRUD Last - write Update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Task from "./Task";
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
+  // const [completed, setCompleted] = useState(false);
 
   const handleChange = (event) => {
     setNewTask(event.target.value);
@@ -14,9 +15,25 @@ const App = () => {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     };
     const newTodoList = [...todoList, task];
 
+    setTodoList(newTodoList);
+  };
+
+  const completeTask = (id) => {
+    const newTodoList = todoList.map((task) => {
+      if (task.id === id) {
+        // setCompleted(!completed);
+        // task.completed = !task.completed;
+        // return task;
+        //객체의 일부 요소의 값을 바꿀 때, 스프레드 연산자로
+        //키값을 나열하고, 바꿀 요소를 덧붙여서 덮어쓰기 한다.....
+        return { ...task, completed: true };
+      }
+      return task;
+    });
     setTodoList(newTodoList);
   };
 
@@ -45,7 +62,9 @@ const App = () => {
               <Task
                 taskName={task.taskName}
                 id={task.id}
+                completed={task.completed}
                 deleteTask={deleteTask}
+                completeTask={completeTask}
               />
             );
           })}
--- src/Task.js
+++ src/Task.js
@@ -3,9 +3,12 @@ import React from "react";
 const Task = (props) => {
   return (
     <div className="task">
-      <h1>
+      <h1 style={{ color: props.completed ? "red" : "black" }}>
         {props.taskName} - {props.id}
       </h1>
+      <button onClick={() => props.completeTask(props.id)}>
+        {props.completed ? "Completed" : "Not Yet"}
+      </button>
       <button onClick={() => props.deleteTask(props.id)}>X</button>
     </div>
   );
