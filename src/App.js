228679dbbdbe45511ba405e486b2fa6607b6bbfd
rndhldlxp <autarkist@naver.com>
2023-02-04 21:03:51 +0900

    crud I with array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
+  const deleteTask = (taskName) => {
+    const newTodoList = todoList.filter((task) => {
+      // return task !== taskName;
+      if (task === taskName) {
+        return false; // this task doesn't want keeping
+      } else {
+        return true; // this task wants keeping
+      }
+    });
+    setTodoList(newTodoList);
+  };
 
   return (
     <>
       <div className="App">
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
-        <button onClick={() => setCount(0)}>Set to Zero</button>
+        <div className="addTask">
+          <input onChange={handleChange} />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        <div className="list">
+          {todoList.map((task) => {
+            return (
+              <div>
+                <h1>{task}</h1>
+                <button onClick={() => deleteTask(task)}>X</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="App">{count}</div>
     </>
   );
 };
